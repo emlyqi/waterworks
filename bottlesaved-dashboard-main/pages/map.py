f2e03b51b9e8486980e4c29526138c0d076019aa
Angela Zhuang <angelazhuang@Angelas-MacBook-Air.local>
2024-09-28 16:32:11 -0400

    excellent new maps

--- bottlesaved-dashboard-main/pages/map.py
+++ bottlesaved-dashboard-main/pages/map.py
@@ -1,15 +1,41 @@
 # page1.py
 import streamlit as st
-import pandas as pd
+
+import folium
+from streamlit_folium import st_folium
+
+
 if "shared" not in st.session_state:
    st.session_state["shared"] = True
 
-data = {
-    'latitude': [37.7749, 34.0522, 40.7128],
-    'longitude': [-122.4194, -118.2437, -74.0060],
-    'city': ['San Francisco', 'Los Angeles', 'New York']
-}
-df = pd.DataFrame(data)
+m = folium.Map(location=[43.471041, -80.541412], zoom_start=15)
+
+folium.TileLayer('openstreetmap').add_to(m)
+folium.TileLayer('CartoDB dark_matter').add_to(m)
+folium.TileLayer('CartoDB positron').add_to(m)
+folium.LayerControl().add_to(m)
+
+
+# Sample data for locations
+locations = [
+    {"name": "fountain1", "lat": 43.471041, "lon": -80.541412},
+    {"name": "fountain2", "lat": 43.471041, "lon": -80.530130},
+    {"name": "fountain3", "lat": 43.471041, "lon": -80.490233},
+]
+
+# st.
+
+# Optional: Add a custom CircleMarker for precision
+for loc in locations:
+    folium.CircleMarker(
+        location=[loc['lat'], loc['lon']],
+        radius=3,  # Adjust the size here
+        color='blue',
+        fill=True,
+        fill_color='blue',
+        fill_opacity=0.6,
+        popup=loc['name']
+    ).add_to(m)
 
 # Display the map
-st.map(df)
+st_folium(m, width=700)
