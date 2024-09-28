2e66afee2e08d0524d43a10495680ec9966a66f9
Angela Zhuang <angelazhuang@Angelas-MacBook-Air.local>
2024-09-28 15:30:16 -0400

    new map

--- bottlesaved-dashboard-main/pages/map.py
+++ bottlesaved-dashboard-main/pages/map.py
@@ -1,4 +1,15 @@
 # page1.py
 import streamlit as st
+import pandas as pd
 if "shared" not in st.session_state:
    st.session_state["shared"] = True
+
+data = {
+    'latitude': [37.7749, 34.0522, 40.7128],
+    'longitude': [-122.4194, -118.2437, -74.0060],
+    'city': ['San Francisco', 'Los Angeles', 'New York']
+}
+df = pd.DataFrame(data)
+
+# Display the map
+st.map(df)
